6404d8ce87581e207752548caf91cebcd0a7281d
Marc Baqués Sàbat <marc.baques@enti.cat>
2021-03-11 19:26:06 +0100

    Adding funcionality

--- MasterMind/ViewModel.swift
+++ MasterMind/ViewModel.swift
@@ -11,8 +11,26 @@ import SwiftUI
 class ViewModel {
     
     let secretCode : CodeModel = CodeModel()
+    let configuration:Configuration = Configuration()
+    var codeGuesses: [CodeModel] = []
+    var codeAnswers:[CodeModel] = []
+    var actualTurn : Int = 1
     
+    func SetupGame(){
+        
+        for <#item#> in <#items#> {
+            <#code#>
+        }
+        
+    }
     
+    func CheckGuess(){
+        
+    }
+    
+    func WinGame(){
+        
+    }
     
     func GenerateSecretColor(){
         
