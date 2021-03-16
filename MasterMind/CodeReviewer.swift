7717cfc9be182fd421993b1066e9339985d98b9f
Marc Baqués Sàbat <36268802+MarcBaquesSabat@users.noreply.github.com>
2021-03-16 14:24:03 +0100

    Added code review logic

--- MasterMind/CodeReviewer.swift
+++ MasterMind/CodeReviewer.swift
@@ -10,8 +10,51 @@ import SwiftUI
 
 public class CodeReviewer{
     
-    func ReviewCode(_ guess:CodeModel, _ secretCode:CodeModel){
+    func ReviewCode(_ guess:[Color], _ secretCode:[Color]) -> (positioned : Int, missPositioned : Int){
+        var matches = 0
+        var missplace = 0
         
+        var guessDict : [Color:Int] = [:]
+        var secretDict : [Color:Int] = [:]
+        for (index,color) in guess.enumerated() {
+
+            if(guess[index] == secretCode[index]){
+                matches += 1
+            }
+            else
+            {
+                if(guessDict[guess[index]] == nil){
+                    guessDict[guess[index]] = 1
+                }
+                else
+                {
+                    guessDict[guess[index]]! += 1
+                }
+
+                if(secretDict[secretCode[index]] == nil){
+                    secretDict[secretCode[index]] = 1
+                }
+                else
+                {
+                    secretDict[secretCode[index]]! += 1
+                }
+            }
+        }
+        for(key,value) in guessDict{
+            if(secretDict[key] == nil){
+                continue
+            }
+            
+            if(guessDict[key]! > secretDict[key]!){
+                missplace += secretDict[key]!
+            }
+            else
+            {
+                missplace += guessDict[key]!
+            }
+        }
+
+        return (matches,missplace)
     }
     
 }
